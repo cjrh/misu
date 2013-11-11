7ca6dc0312f482e806e598002d7dfef64ac72605
Caleb Hattingh <caleb.hattingh@gmail.com>
2013-11-11 15:44:19 +1000

    Changed benchmark to repeat code more (to abuse freelist?).

--- lies_and_damnedlies.py
+++ lies_and_damnedlies.py
@@ -8,26 +8,33 @@ Created on Wed Sep 04 14:55:02 2013
 @author: caleb.hattingh
 """
 from __future__ import absolute_import, division, print_function, unicode_literals
-import timeit 
+import timeit
 
 unity_setup = '''import unity as u
 def calc(mass, volume, time):
-    return mass * volume / time
+    for x in xrange(1000):
+        y = mass * volume / time * x / 10000.
+    return y
 '''
 unity_code = '''calc(10.0*u.kg, 5.0*u.m**3, 20.0*u.second)'''
 
 pq_setup = '''import quantities as pq
 def calc(mass, volume, time):
-    return mass * volume / time
+    for x in xrange(1000):
+        y = mass * volume / time * x / 10000.
+    return y
 '''
 pq_code = '''calc(10.0*pq.kg, 5.0*pq.m**3, 20.0*pq.second)'''
 
-plain_setup = '''def calc(mass, volume, time):
-    return mass * volume / time
+plain_setup = '''
+def calc(mass, volume, time):
+    for x in xrange(1000):
+        y = mass * volume / time * x / 10000.
+    return y
 '''
 plain_code = '''calc(10.0, 5.0, 20.0)'''
 
-COUNT = 10000
+COUNT = 10
 
 # Unity
 print('{:15}: {:>8.4g} s'.format(
