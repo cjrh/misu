fb918cd5832a5fab1a973c12e06458ea40d3e71b
Caleb Hattingh <caleb.hattingh@gmail.com>
2015-02-27 23:07:27 +1000

    More fields into setup.py.

--- setup.py
+++ setup.py
@@ -39,11 +39,15 @@ setup(
         'License :: OSI Approved :: MIT License',
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
         ],
     # What does your project relate to?
     keywords='math science engineering physics quantities units',
     packages=['misu'],
+    install_requires=['cython','numpy'],
     ext_modules = cythonize("misu/*.pyx"),
     include_dirs=[numpy.get_include()]
     )
