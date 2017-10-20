1712314075972402a3041d7617046c7248704707
Caleb Hattingh <caleb.hattingh@gmail.com>
2017-10-20 23:08:42 +1000

    Fix many tiny formatting problems in setup.py.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 # Always prefer setuptools over distutils
+from os import path
+from codecs import open  # To use a consistent encoding
 from setuptools import setup
 from Cython.Build import cythonize
 import numpy
-from codecs import open # To use a consistent encoding
-from os import path
 
 # Get the long description from the relevant file
-here=path.abspath(path.dirname(__file__))
+here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description=f.read()
+    long_description = f.read()
 
 setup(
     name='misu',
@@ -18,37 +18,25 @@ setup(
     version='1.0.3',
     description='Fast quantities',
     long_description=long_description,
-    #this is the project's main homepage.
     url='https://github.com/cjrh/misu',
-    # Author details
     author='Caleb Hattingh',
     author_email='caleb.hattingh@gmail.com',
-    # Choose your license
     license='MIT',
-    # See https://PyPI.python.org/PyPI?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
-        # Indicate who your project is intended for
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
-        # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        ],
-    # What does your project relate to?
+        'Programming Language :: Python :: 3.6',
+    ],
     keywords='math science engineering physics quantities units',
     packages=['misu'],
-    install_requires=['cython','numpy'],
-    ext_modules = cythonize("misu/*.pyx"),
+    install_requires=['cython', 'numpy'],
+    ext_modules=cythonize("misu/*.pyx"),
     include_dirs=[numpy.get_include()]
-    )
+)
